3753c71992fec4cfc219719943af57a4767eead7
prathameshchaskar <prathameshchaskar1@gmail.com>
2025-01-06 01:33:41 +0530

    application properties updated

--- src/main/java/com/prathamesh/ECommerceBackendApplication.java
+++ src/main/java/com/prathamesh/ECommerceBackendApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.prathamesh.controller"})
+@ComponentScan(basePackages = "com.prathamesh.config,"+"com.prathamesh.controller,"+"com.prathamesh.exception,"+
+"com.prathamesh.model,"+"com.prathamesh.repository,"+"com.prathamesh.request,"+"com.prathamesh.response,"+"com.prathamesh.service")
 public class ECommerceBackendApplication {
 
 	public static void main(String[] args) {
